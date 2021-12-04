da7d1c6a1cebc598c52fe1b499f5d995de8b898f
Josiah <josiah.v@live.com>
2021-12-04 18:29:11 -0500

    new file:   keep_alive.py 	modified:   shinjitweets.py

--- shinjitweets.py
+++ shinjitweets.py
@@ -1,42 +1,49 @@
 from requests_oauthlib import OAuth1Session
 import json
 import line_generator
+import keep_alive
 
 
 
-keys = open("C:\\Users\\josiah\\dev\\shinjitweets\\actual-keys.txt", "r").read().split("\n")
+def main():
+    keys = open("C:\\Users\\josiah\\dev\\shinjitweets\\actual-keys.txt", "r").read().split("\n")
 
-consumer_key = keys[0]
-consumer_secret = keys[1]
-access_token = keys[2]
-access_token_secret = keys[3]
+    consumer_key = keys[0]
+    consumer_secret = keys[1]
+    access_token = keys[2]
+    access_token_secret = keys[3]
 
 
-# Generate Shinji tweet
-text = line_generator.gen_line()
-payload = {"text": text}
+    # Generate Shinji tweet
+    text = line_generator.gen_line()
+    payload = {"text": text}
 
-# Make the request
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=access_token,
-    resource_owner_secret=access_token_secret,
-)
-
-# Making the request
-response = oauth.post(
-    "https://api.twitter.com/2/tweets",
-    json=payload,
-)
-
-if response.status_code != 201:
-    raise Exception(
-        "Request returned an error: {} {}".format(response.status_code, response.text)
+    # Make the request
+    oauth = OAuth1Session(
+        consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret,
     )
 
-print("Response code: {}".format(response.status_code))
+    # Making the request
+    response = oauth.post(
+        "https://api.twitter.com/2/tweets",
+        json=payload,
+    )
 
-# Saving the response as JSON
-json_response = response.json()
-print(json.dumps(json_response, indent=4, sort_keys=True))
+    if response.status_code != 201:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+
+    print("Response code: {}".format(response.status_code))
+
+    # Saving the response as JSON
+    json_response = response.json()
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+# Keep a web server alive
+keep_alive()
+
+main()
